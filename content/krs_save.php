1d0c69518033a70952f1d45227efd21389eccdbe
Rettorio <ardiansyahrukua07@gmail.com>
2024-02-14 06:14:06 +0900

    update krs_save with new closure from function

--- content/krs_save.php
+++ content/krs_save.php
@@ -3,11 +3,11 @@ require_once "config/koneksi.php";
 require_once __DIR__ . "/../function.php";
 
 //validasi user
-if(!isAuth()) {
+if (!isAuth()) {
     redirectTo("login.php");
 }
 
-if(empty($_POST['nim']) || empty($_POST['id_smt'])) {
+if (empty($_POST['nim']) || empty($_POST['id_smt'])) {
     flashMessage("danger", "Invalid operation.");
     back();
 }
@@ -16,15 +16,15 @@ $nim = htmlspecialchars($_POST['nim']);
 $id_smt = htmlspecialchars($_POST['id_smt']);
 
 //select matkul yang sudah ada di table krs sesuai semester aktif
-$selectedMatkulQ = "SELECT kode_mk FROM krs WHERE nim = '$nim' AND id_smt = '$id_smt';"; 
-$matkulTerpilih = array_map(fn($r) => $r["kode_mk"], $dbFetcher($selectedMatkulQ));
+$selectedMatkulQ = "SELECT kode_mk FROM krs WHERE nim = ? AND id_smt = ?;";
+$matkulTerpilih = $dbFetchWithMode($selectedMatkulQ, PDO::FETCH_COLUMN, array($nim, $id_smt));
 //filter matakuliah dari $_POST
 $matkulPilihan = array_filter($_POST, function ($key) {
     return strpos($key, "matkul") !== false;
 }, ARRAY_FILTER_USE_KEY);
 
 //validasi $matkulPilihan ada atau tidak
-if(empty($matkulPilihan)) {
+if (empty($matkulPilihan)) {
     flashMessage("warning", "Tidak ada matakuliah yang dipilih.");
     redirectTo("?page=krs");
 }
@@ -32,12 +32,12 @@ if(empty($matkulPilihan)) {
 foreach ($matkulPilihan as $matkulKelas) {
     list($kode_mk, $id_kelas) = explode("-", $matkulKelas);
     //validasi apakah matkul yg dipilih sudah ada dalam daftar krs smt aktif
-    if(in_array($kode_mk, $matkulTerpilih)) {
+    if (in_array($kode_mk, $matkulTerpilih)) {
         flashMessage("warning", "Matakuliah sudah diambil!");
         back();
     }
     //insert table krs
-    $dbPush("krs", compact('nim','kode_mk','id_smt','id_kelas'));
+    $dbPush("krs", compact('nim', 'kode_mk', 'id_smt', 'id_kelas'));
 }
 
 flashMessage("success", "Berhasil menambah Matakuliah.");
--- function.php
+++ function.php
@@ -1,27 +1,33 @@
 <?php
+
 declare(strict_types=1);
 require_once "config/koneksi.php";
 
-function flashMessage(string $type, $msg) :void {
+function flashMessage(string $type, $msg): void
+{
     $alert = ["alertType" => $type, "alertMessage" => $msg];
     $_SESSION['alert'] = $alert;
     $_SESSION['alert-shown'] = false;
 }
 
-function setOldInput(string $name,$value) :void {
+function setOldInput(string $name, $value): void
+{
     $old = [$name => $value];
     $_SESSION['oldinput'] = $old;
 }
 
-function clearOldInput() :void {
+function clearOldInput(): void
+{
     unset($_SESSION['oldinput']);
 }
 
-function consoleMessage(string $msg) :void {
+function consoleMessage(string $msg): void
+{
     echo "<script>console.log('$msg');</script>";
 }
 
-function alertIconGenerator(string $type) :string {
+function alertIconGenerator(string $type): string
+{
     switch ($type) {
         case 'info':
             return '<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>';
@@ -31,53 +37,67 @@ function alertIconGenerator(string $type) :string {
             return '<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>';
         case 'danger':
             return '<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>';
-        }
-
     }
-        
-function getFirstName(string $name) {
+}
+
+function getFirstName(string $name)
+{
     return explode(" ", $name)[0];
 }
 
-function back() :void {
+function back(): void
+{
     $destination = $_SERVER['HTTP_REFERER'];
     header("Location: $destination");
     exit(0);
 }
 
-function redirectTo(string $dest) :void {
+function redirectTo(string $dest): void
+{
     header("Location: $dest");
-    exit(0);    
+    exit(0);
 }
 
-function isAuth() :bool {
+function isAuth(): bool
+{
     return isset($_SESSION['auth']) || isset($nim) || isset($nama);
 }
 
-function showAlert(string $type, $message) :void {
+function showAlert(string $type, $message): void
+{
     $icon = alertIconGenerator($type);
 
-    echo '<div class="alert alert-'.$type.' d-flex align-items-center py-2 alert-space" role="alert">
-    '.$icon.'<div>'.
-    $message.
-    '</div></div>';
+    echo '<div class="alert alert-' . $type . ' d-flex align-items-center py-2 alert-space" role="alert">
+    ' . $icon . '<div>' .
+        $message .
+        '</div></div>';
 
     $_SESSION['alert-shown'] = true;
 }
 
-    
-$dbFetcher = function(string $query, bool $fetchAll = true, array $param = []) use($con) :array {
+
+$dbFetcher = function (string $query, bool $fetchAll = true, array $param = []) use ($con): array {
     try {
-        $statement = $con->prepare($query); 
+        $statement = $con->prepare($query);
         $statement->execute($param);
         $result = $fetchAll ? $statement->fetchAll() : $statement->fetch();
-        return $result; 
+        return $result;
     } catch (\PDOException $th) {
         throw $th;
     }
 };
 
-$dbPush = function(string $table, array $data) use ($con) :bool {
+$dbFetchWithMode = function (string $query, int $mode, array $param) use ($con): array {
+    try {
+        $statement = $con->prepare($query);
+        $statement->execute($param);
+        return $statement->fetchAll($mode);
+    } catch (\PDOException $th) {
+        throw $th;
+    }
+};
+
+$dbPush = function (string $table, array $data) use ($con): bool {
     try {
         $columns = implode(',', array_keys($data));
         $values = '"' . join('","', array_values($data)) . '"';
@@ -88,13 +108,13 @@ $dbPush = function(string $table, array $data) use ($con) :bool {
     }
 };
 
-$dbDestroy = function(string $table, array $id) use($con) :bool {
+$dbDestroy = function (string $table, array $id) use ($con): bool {
     try {
         $prim = array_keys($id)[0];
         $idPrim = array_values($id)[0];
         $statement = $con->prepare("DELETE FROM $table WHERE $prim = $idPrim;");
         return $statement->execute();
-    } catch (\PDOException $th) {   
+    } catch (\PDOException $th) {
         throw $th;
     }
 };
